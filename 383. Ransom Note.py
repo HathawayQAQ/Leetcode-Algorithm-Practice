468711980eb70ba14eef649e5784849aea19431c
HathawayQAQ <94154940+HathawayQAQ@users.noreply.github.com>
2024-03-05 23:45:21 -0500

    RansomNote

---
+++ 383. Ransom Note.py
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+       HashMap<Character, Integer> map = new HashMap<>();
+       for (char c : magazine.toCharArray()) {
+           map.put(c, map.getOrDefault(c, 0) + 1);
+       }
+
+       for (char c : ransomNote.toCharArray()) {
+           if (!map.containsKey(c) || map.get(c) < 1) {
+               return false;
+           }
+           map.put(c, map.get(c) - 1);
+       }
+
+       return true;
+    }
+}
